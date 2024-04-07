9401d94768d0806cf9ac07cb79db5d586d787f5b
YlarHeartbb <202080010@psu.palawan.ph>
2024-04-07 11:52:42 +0800

    modify theme ready bootstrap and install django-widget-tweaks

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from studentorg.models import Organization
 
+class HomePageView(ListView):
+    model = Organization
+    contextobject_name = 'home'
+    template_name = "home.html"
 # Create your views here.
